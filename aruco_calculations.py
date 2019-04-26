ea7ed4e9850982dfc4579a3f372983b94b0ecba0
Kelly Freet <kelly.freet13@yahoo.com>
2019-04-26 14:29:53 -0700

    getting ready for threading

--- aruco_calculations.py
+++ aruco_calculations.py
@@ -1,12 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 import cv2.aruco as aruco
 import glob
 import time
 import json
-import math
-from transforms3d.euler import mat2euler, euler2mat
+import io
+import subprocess
 
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -14,6 +13,13 @@ from picamera import PiCamera
 # size in meters of aruco marker
 ARUCO_SQUARE_WIDTH = 0.141  # formerly 0.152
 CALIB_FILENAME = 'camera_calib.json'
+
+# shared global variables accessed from outside
+#global X
+#global Z
+#X = 0
+#Z = 0
+marker_id_to_find = 1
 
 
 def video_debugging():
@@ -89,34 +95,60 @@ def video_debugging():
         raw_capture.truncate(0)
 
 
-def single_frame_continuous_capture(continual_capture_seconds):
+def single_frame_continuous_capture(continual_capture_seconds=2):
     print('using pi camera single frame capture')
 
-    # test camera init time
-    start = time.time()
-    with PiCamera() as camera:
-        pass
-    print('camera startup time', time.time()-start)
+    # test globals
+    global X
+    global Z
 
     # init the camera and grab a reference to the raw camera capture
     camera = PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 32
-    raw_capture = PiRGBArray(camera, size=(640, 480))
+    camera.resolution = (2592, 1952)
+    #camera.framerate = 32
+    raw_capture = PiRGBArray(camera, size=(2592, 1952))
 
     # allow camera to warm up -> tested safe value
-    time.sleep(0.2)
+    time.sleep(0.3)
 
     # create aruco marker dictionary and get camera calibration
     marker_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
     camera_mtx, dist_coeffs = load_camera_calibration(CALIB_FILENAME)
 
     while True:
+
+        # check if we've been given a marker to find
+        if marker_id_to_find is None:
+            time.sleep(continual_capture_seconds)
+            print('no marker set to find. sleeping')
+            continue
+
+        # setup byte stream
+        stream = io.BytesIO()
+
         # grab an image from the camera and get numpy arr
+        #camera.capture(stream, format='bgr')
+
+        #img_arr = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        #img = cv2.imdecode(img_arr, 1)
         camera.capture(raw_capture, format="bgr")
-        image = raw_capture.array
-        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        u_frame = cv2.UMat(img_gray)
+        img = raw_capture.array
+
+        fname = 'cv2image.jpg'
+        cv2.imwrite(fname, img)
+        #image = cv2.imread(fname, 0)
+
+        # some sketch shit
+        subprocess.run(['convert', fname, '-resize', '3280x2464', fname])
+        image = cv2.imread(fname, 0)
+
+        #cv2.imshow('image', image)
+        #cv2.waitKey(0)
+        u_frame = cv2.UMat(image)
+
+        # clean up
+        stream.seek(0)
+        stream.truncate()
 
         # detect the aruco marker
         corners, ids, rejectedImgPoints = aruco.detectMarkers(u_frame, marker_dict)
@@ -151,19 +183,37 @@ def single_frame_continuous_capture(continual_capture_seconds):
                 # extract x offset and z camera to marker distance
                 z = cam_pose[-1]
                 x = tvec[0][0]
+
+                # testing an idea here
+                z *= .79024
+                x *= .78896
                 z = round(z, 3)
                 x = round(x, 3)
                 z_fmt = 'z: {0} meters'.format(z)
                 x_fmt = 'x: {0} meters'.format(x)
+
+                # set to global
+                X = x
+                Z = z
 
                 # draw detected markers on frame
                 aruco.drawDetectedMarkers(u_frame, corners, ids)
                 posed_img = aruco.drawAxis(u_frame, u_camera_mtx, u_dist_coeffs, rvec, tvec, 0.1)
 
                 # draw x and z distance calculation on frame
-                cv2.putText(posed_img, z_fmt, (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (10,10,10))
-                cv2.putText(posed_img, x_fmt, (0, 80), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (10,10,10))
-                cv2.imshow("aruco detection", posed_img)
+                cv2.namedWindow('aruco', cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('aruco', 600, 600)
+                cv2.putText(posed_img, z_fmt, (260, 290), cv2.FONT_HERSHEY_SIMPLEX, 5.0, (10,10,10))
+                cv2.putText(posed_img, x_fmt, (260, 450), cv2.FONT_HERSHEY_SIMPLEX, 5.0, (10,10,10))
+
+                save_fname = './live_images/'+str(X)+str(Z)+'.jpg'
+                cv2.imwrite(save_fname, posed_img)
+                #cv2.resizeWindow('aruco', 600, 600)
+                #cv2.waitKey(0)
+
+                # confirm with console
+                #print(z_fmt)
+                #print(x_fmt)
 
                 # clean up and exit condition
                 key = cv2.waitKey(1) & 0XFF
@@ -171,11 +221,12 @@ def single_frame_continuous_capture(continual_capture_seconds):
                 if key == ord("q"):
                     break
 
+        cv2.destroyAllWindows()
         # execute this every second
         sleep_start = time.time()
         time.sleep(continual_capture_seconds)
         sleep_end = time.time()
-        print('slept: {0} seconds'.format(sleep_end-sleep_start))
+        print('[aruco_calc.py] global x: {0}, z: {1}'.format(X, Z))
 
 
 def test_aruco_image_folder(dir, expression):
@@ -190,7 +241,7 @@ def test_aruco_image_folder(dir, expression):
         print(fname)
         image = cv2.imread(fname, 0)
         u_frame = cv2.UMat(image)
-
+        print('image shape: ', image.shape)
         # detectMarkers with frame, marker dict, marker corners, and marker ids
         corners, ids, _ = aruco.detectMarkers(u_frame, marker_dict)
 
@@ -258,10 +309,12 @@ if __name__ == "__main__":
     x_offset_reg = 'x_offset*.jpg'
     z_dist_reg = 'meter*.jpg'
     xz_test_dir = './aruco_imgs/'
-    test_aruco_image_folder(xz_test_dir, z_dist_reg)
+    test_dir_apr = './test_images_Apr_25/'
+    all_reg = '*.jpg'
+    #test_aruco_image_folder(test_dir_apr, all_reg)
 
     # continual single frame capture
     capture_every_n_seconds = 2
-    #single_frame_continuous_capture(capture_every_n_seconds)
+    single_frame_continuous_capture(capture_every_n_seconds)
 
     #video_debugging()
